d8f31ae83639a4caeed94dc9835d30e43ad3553c
dahono22 <dahono22@bemen3.cat>
2025-04-01 19:07:22 +0200

    feat: afegeix model Oferta amb titol, descripció, empresa i ubicació

---
+++ conecta/lib/models/oferta.dart
@@ -0,0 +1,16 @@
+// lib/models/oferta.dart
+class Oferta {
+  final String id;
+  final String titol;
+  final String descripcio;
+  final String ubicacio;
+  final String empresa;
+
+  Oferta({
+    required this.id,
+    required this.titol,
+    required this.descripcio,
+    required this.ubicacio,
+    required this.empresa,
+  });
+}
