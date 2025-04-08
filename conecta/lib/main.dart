47e1e257b14254e616d1859386dc936f08664777
dahono22 <dahono22@bemen3.cat>
2025-04-08 18:34:01 +0200

    chore: integració inicial de Firebase SDK amb Firestore

--- conecta/lib/main.dart
+++ conecta/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart'; // 🔥 Importación de Firebase
+
 import 'app.dart';
 import 'services/auth_service.dart';
 import 'services/offer_service.dart';
 import 'services/offer_application_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // 🧩 Necesario para inicializar Firebase antes de correr la app
+  await Firebase.initializeApp(); // 🚀 Inicialización de Firebase
+
   runApp(
     MultiProvider(
       providers: [
