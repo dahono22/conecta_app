9632832ab8a37630eb23af80cf7692f59a21ccd9
dahono22 <dahono22@bemen3.cat>
2025-04-22 16:06:30 +0200

    feat(models): afegeix model Oferta amb conversió des de i cap a Map

--- conecta/lib/models/oferta.dart
+++ conecta/lib/models/oferta.dart
@@ -1,16 +1,49 @@
 // lib/models/oferta.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Oferta {
   final String id;
+  final String empresaId;
   final String titol;
   final String descripcio;
+  final String requisits;
   final String ubicacio;
-  final String empresa;
+  final DateTime dataPublicacio;
+  final String estat; // pendent, publicada, rebutjada
 
   Oferta({
     required this.id,
+    required this.empresaId,
     required this.titol,
     required this.descripcio,
+    required this.requisits,
     required this.ubicacio,
-    required this.empresa,
+    required this.dataPublicacio,
+    required this.estat,
   });
+
+  factory Oferta.fromMap(Map<String, dynamic> data, String id) {
+    return Oferta(
+      id: id,
+      empresaId: data['empresaId'] ?? '',
+      titol: data['titol'] ?? '',
+      descripcio: data['descripcio'] ?? '',
+      requisits: data['requisits'] ?? '',
+      ubicacio: data['ubicacio'] ?? '',
+      dataPublicacio: (data['dataPublicacio'] as Timestamp).toDate(),
+      estat: data['estat'] ?? 'pendent',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'empresaId': empresaId,
+      'titol': titol,
+      'descripcio': descripcio,
+      'requisits': requisits,
+      'ubicacio': ubicacio,
+      'dataPublicacio': dataPublicacio,
+      'estat': estat,
+    };
+  }
 }
